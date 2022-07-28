1ab1dbf1e2c071b7d6e2bf4a501a782900a82995
Gatteber <Garrett.Atteberry@gmail.com>
2022-07-29 00:33:13 +0900

    added rendering for project and add bar, fixed bugs on scss.

--- src/navbar.js
+++ src/navbar.js
@@ -6,21 +6,13 @@ const navBarControl = (() => {
 
     const open = (buttonitem) => {
         buttonitem.classList.add('active');
-        // button.classList.add('active');
-        // domManip.makeEl(renderPage.getContent, 'div', 'nav-add-menu');
-        // const getAddMenuBody = domManip.grabEl('nav-add-menu');
-        // domManip.makeEl(getAddMenuBody, 'div', 'nav-menu-add-i', "+ New Item");
-        // domManip.makeEl(getAddMenuBody, 'div', 'nav-menu-add-p', "+ New Project");
     }
 
     const close = (buttonitem) => {
         buttonitem.classList.remove('active');
-        // const getAddMenuBody = domManip.grabEl('nav-add-menu');
-        // getAddMenuBody.parentNode.removeChild(getAddMenuBody);
     }
 
     return { isActive, open, close, }
-
 })();
 
 //nav bar event module
@@ -30,10 +22,62 @@ const navBarEvent = (() => {
             if(!navBarControl.isActive) {
                 navBarControl.open(item);
                 navBarControl.isActive = true;
+                renderNavItem.render(item);
+                setTimeout(() => {
+                    if (navBarControl.isActive) {
+                        navBarControl.close(item);
+                        navBarControl.isActive = false;
+                        renderNavItem.delRender();
+                    }
+                }, "5000");
+
             } else if (navBarControl.isActive) {
                 navBarControl.close(item);
                 navBarControl.isActive = false;
+                renderNavItem.delRender();
             }
         })
     })
+})();
+
+const renderNavItem = (() => {
+    //check for each item and render it
+    const render = (domElement) => {
+        if (domElement.classList == "nav-add active") {
+            domManip.makeEl(renderPage.getContent, 'div', 'nav-add-menu');
+            const getAddMenuBody = domManip.grabEl('nav-add-menu');
+            domManip.makeEl(getAddMenuBody, 'div', 'nav-menu-add-i', "+ New Item");
+            domManip.makeEl(getAddMenuBody, 'div', 'nav-menu-add-p', "+ New Project");
+
+        } else if (domElement.classList == "nav-today active") {
+            //render today's tasks
+            console.log("i'm under construction!");
+            navBarControl.close(domElement);
+
+        } else if (domElement.classList == "nav-week active") {
+            //render this week's tasks
+            console.log("i'm under construction too!");
+            navBarControl.close(domElement);
+
+        } else if (domElement.classList == "nav-projects active") {
+            //render nav projects
+            console.log("i'm being worked on!");
+            domManip.makeEl(renderPage.getContent, 'div', 'nav-proj-menu');
+            const getProjMenuBody = domManip.grabEl('nav-proj-menu');
+            domManip.makeEl(getProjMenuBody, 'div', 'proj-item', "All Projects");
+            domManip.makeEl(getProjMenuBody, 'div', 'proj-item', "Test Project");
+        }
+    };
+
+    const delRender = () => {
+        const getAddMenuBody = domManip.grabEl('nav-add-menu');
+        const getProjMenuBody = domManip.grabEl('nav-proj-menu');
+        if (getAddMenuBody !== null) {
+            getAddMenuBody.parentNode.removeChild(getAddMenuBody);
+        } else if (getProjMenuBody !== null) {
+            getProjMenuBody.parentNode.removeChild(getProjMenuBody);
+        }
+    }
+
+    return { render, delRender };
 })();
